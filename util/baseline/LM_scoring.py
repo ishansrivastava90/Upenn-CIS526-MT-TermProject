32d06b837501059e028e88c1ce8c88f6f9e297dc
mounicam <mmaddela1005@gmail.com>
2015-04-07 19:40:01 -0400

    Brevity penalty for lm scoring

--- util/baseline/LM_scoring.py
+++ util/baseline/LM_scoring.py
@@ -4,6 +4,7 @@ import optparse
 from numpy import argmax,argmin,mean
 from nltk import word_tokenize;
 import editdistance
+from math import exp
 
 optparser = optparse.OptionParser()
 optparser.add_option("-i", "--input", dest="input", default="../../data/turk_translations_w_logprob_eurparl.tsv", help="MTurk translations file with language model probabilities")
@@ -19,10 +20,14 @@ all_hyps = [line.split('\t')[2:] for line in open(opts.input)]
 
 for (ind,hyp) in enumerate(all_hyps[1:]) :
     sents = hyp[0:4];
-    bprob = [ float(h) for h in hyp[4:8] ];
-    tprob = [ float(h) for h in hyp[8:12] ];
-    for (ind,sent) in enumerate(sents) :
+    avg_len = mean([len(s) for s in sents ]);
+    penalty = [ exp(abs(len(s)-avg_len)*1.0 / avg_len) for s in sents ]
+    bprob = [ float(h)*penalty[ind] for (ind,h) in enumerate(hyp[4:8]) ];
+    tprob = [ float(h)*penalty[ind] for (ind,h) in enumerate(hyp[8:12]) ];
 
+    #bprob = [ float(h) for h in hyp[4:8] ];
+    #tprob = [ float(h) for h in hyp[8:12] ];
+    for (ind,sent) in enumerate(sents) :
          if len(word_tokenize(sent)) <= 2 or "NO TRANSLATION FOUND" in sent:
              bprob[ind] = -10000;
              tprob[ind] = -10000;
