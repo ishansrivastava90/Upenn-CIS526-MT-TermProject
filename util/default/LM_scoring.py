2b292422808469b86ebf098c9df2711f85cfa047
mounicam <mmaddela1005@gmail.com>
2015-04-07 18:32:39 -0400

    language model and edit distance scoring

--- util/default/LM_scoring.py
+++ util/default/LM_scoring.py
@@ -1,24 +1,31 @@
-#!/usr/bin/env python
-
-#############################################################
-## Default System.
 ## Outputs the sentence with the best LM score.
-## BLEU score : 0.1988
+## BLEU score : 0.216
 import optparse
-from numpy import argmax
-
+from numpy import argmax,argmin,mean
+from nltk import word_tokenize;
+import models,editdistance
 
 optparser = optparse.OptionParser()
-optparser.add_option("-i", "--input", dest="input", default="../../data/turk_translations_w_logprob.tsv", help="MTurk translations file with language model probabilities")
-(opts,_) = optparser.parse_args()
+optparser.add_option("-i", "--input", dest="input", default="../../data/turk_translations_w_logprob_eurparl.tsv", help="MTurk translations file with language model probabilities")
 
-wbp = 0.5 ;
-wtp = 0.5;
+
+opts = optparser.parse_args()[0]
+
+
+wbp = 0.0 ;
+wtp = 1.0;
 
 all_hyps = [line.split('\t')[2:] for line in open(opts.input)]
-for hyp in all_hyps[1:] :
+
+for (ind,hyp) in enumerate(all_hyps[1:]) :
+    sents = hyp[0:4];
     bprob = [ float(h) for h in hyp[4:8] ];
     tprob = [ float(h) for h in hyp[8:12] ];
+    for (ind,sent) in enumerate(sents) :
+
+         if len(word_tokenize(sent)) <= 2 or "NO TRANSLATION FOUND" in sent:
+             bprob[ind] = -10000;
+             tprob[ind] = -10000;
+
     max_ind = argmax( [ (wbp*b+ wtp*t) for (b,t) in zip(bprob, tprob) ] );  
-    sents = hyp[0:4];
     print sents[max_ind];
