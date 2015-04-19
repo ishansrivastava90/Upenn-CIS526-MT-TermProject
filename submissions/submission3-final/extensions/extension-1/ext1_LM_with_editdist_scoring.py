9f68dbcdd359ed69a33adea2aa6127b1425f550d
Ishan Srivastava <ishan@seas.upenn.edu>
2015-04-19 00:10:46 -0400

    [CIS526-MT] Changing the data path for the translation file

--- submissions/submission3-final/extensions/extension-1/ext1_LM_with_editdist_scoring.py
+++ submissions/submission3-final/extensions/extension-1/ext1_LM_with_editdist_scoring.py
@@ -18,7 +18,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../util', 'feature_g
 import edit_dist_levenshtein
 
 optparser = optparse.OptionParser()
-optparser.add_option("-i", "--input", dest="input", default="../../data/turk_translations_w_logprob_eurparl_2.tsv", help="MTurk translations file with language model probabilities")
+optparser.add_option("-t", "--trans", dest="trans", default="../../data-ext/translations.tsv", help="MTurk translations file")
+optparser.add_option("-l", "--trans_lm", dest="trans_lm_file", default="../../data-ext/turk_translations_w_logprob_europarl_c.tsv", help="MTurk translations file with language model probabilities")
 
 
 opts = optparser.parse_args()[0]
@@ -27,10 +28,11 @@ opts = optparser.parse_args()[0]
 wbp = 0.1
 wtp = 0.9
 
-all_hyps = [line.split('\t')[2:] for line in open(opts.input)]
+all_sen = [line.split('\t')[6:10] for line in open(opts.trans)]
+all_hyp = [line.split('\t')[2:] for line in open(opts.trans_lm_file)]
 
-for (ind,hyp) in enumerate(all_hyps[1:]) :
-    sents = hyp[0:4]
+for (ind,hyp) in enumerate(all_hyp[1:]) :
+    sents = all_sen[ind]
     avg_ln = mean([len(s) for s in sents ])
     penalty = [ exp(abs(len(s)-avg_ln)*1.0 / avg_ln) for s in sents ]
     bprob = [ float(h)*penalty[ind] for (ind,h) in enumerate(hyp[4:8]) ]
